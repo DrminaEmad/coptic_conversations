0754047b34975088ba3fe7e6497eced8a198c427
DRMINA2019 <drmina2007@yahoo.com>
2026-08-10 20:14:51 +0300

    start timestamp added to the data source

--- coptic_conversations/app/lib/data.ts
+++ coptic_conversations/app/lib/data.ts
@@ -3,6 +3,7 @@ export interface DialogueWord {
   coptic: string;
   english: string;
   arabic: string;
+  startTime?:number;
 }
 
 export interface TranscriptSegment {
@@ -19,7 +20,7 @@ export const conversationData: TranscriptSegment[] = [
     startTime: 0.0,
     endTime: 2.0,
     words: [
-      { coptic: "Ϩⲉⲙⲥⲓ", english: "sit down", arabic: "اجلس" },
+      { coptic: "Ϩⲉⲙⲥⲓ", english: "sit down", arabic: "اجلس", startTime:0.00 },
     ]
   },
   {
@@ -27,7 +28,7 @@ export const conversationData: TranscriptSegment[] = [
     startTime: 3.0,
     endTime: 9.0,
     words: [
-      { coptic: "̀Ⲛⲑⲱⲛ", english: "where", arabic: "اين" },
+      { coptic: "̀Ⲛⲑⲱⲛ", english: "where", arabic: "اين", startTime:0.04 },
     ]
   },
   {
@@ -35,16 +36,16 @@ export const conversationData: TranscriptSegment[] = [
     startTime: 10.0,
     endTime: 14.0,
     words: [
-      { coptic: "Ϩⲉⲙⲥⲓ", english: "set down", arabic: "اجلس" },
-      { coptic: "̀Ⲙⲛⲁⲓ", english: "beside me", arabic: "بجانبي" }    ]
+      { coptic: "Ϩⲉⲙⲥⲓ", english: "set down", arabic: "اجلس", startTime: 0.10 },
+      { coptic: "̀Ⲙⲛⲁⲓ", english: "beside me", arabic: "بجانبي", startTime: 0.11 }    ]
   },
   {
     id: 4,
     startTime: 15.0,
     endTime: 19.0,
     words: [
-      { coptic: "Ϩⲓϫⲉⲛ", english: "on", arabic: "علي" },
-      { coptic: "ⲟⲩ", english: "what", arabic: "ماذا" }
+      { coptic: "Ϩⲓϫⲉⲛ", english: "on", arabic: "علي" , startTime: 0.15},
+      { coptic: "ⲟⲩ", english: "what", arabic: "ماذا", startTime: 0.16}
     ]
   },
   {
@@ -52,8 +53,8 @@ export const conversationData: TranscriptSegment[] = [
     startTime: 23.0,
     endTime: 27.0, 
     words: [
-      { coptic: "Ϩⲓϫⲉⲛ", english: "on", arabic: "علي" },
-      { coptic: "ⲡⲓⲧⲟⲧⲥ", english: "the chair", arabic: "الكرسي" }
+      { coptic: "Ϩⲓϫⲉⲛ", english: "on", arabic: "علي" , startTime: 0.23},
+      { coptic: "ⲡⲓⲧⲟⲧⲥ", english: "the chair", arabic: "الكرسي" , startTime: 0.24}
     ]
   },
 ];
